571af72d49e7557e8a8a3454090275f76820e007
John Roble <roble_john@outlook.com>
2017-10-19 00:48:19 +0800

    Added football registration/tryout form

---
+++ application/models/Football_model.php
@@ -0,0 +1,24 @@
+<?php
+
+	class Football_model extends CI_Model {
+
+		function get_records() {
+			$query = $this->db->get('football');
+			return $query->result();
+		}
+
+		function add_records($data) {
+			$this->db->insert('football', $data);
+			return;
+		}
+
+		function update_records($data) {
+			$this->db->where('id', 14);
+			$this->db->update('football', $data);
+		}
+
+		function delete_row() {
+			$this->db->where('id', $this->url->segment(3));
+			$this->db->delete('football');
+		}
+	}
