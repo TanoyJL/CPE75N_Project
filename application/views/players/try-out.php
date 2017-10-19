8f40b99ebb0694f022a89d426258076d37d7a4a9
Roy Niño Regala <roynregala@gmail.com>
2017-10-19 22:21:24 +0800

    add common form

--- application/views/players/try-out.php
+++ application/views/players/try-out.php
@@ -4,25 +4,25 @@
     <div class="form-group row">
       <label class="control-label col-sm-2" > Name</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" placeholder="Enter first name" name="firstname" required>
+        <input type="text" class="form-control" placeholder="Enter first name" name="first_name" required>
       </div>
     </div>
     <div class="form-group row">
       <label class="control-label col-sm-2 " ></label>
       <div class="col-sm-10">          
-        <input type="text" class="form-control" placeholder="Enter last name" name="lastname" required>
+        <input type="text" class="form-control" placeholder="Enter last name" name="last_name" required>
       </div>
     </div>
      <div class="form-group row">
           <label class="control-label col-sm-2 " >ID Number</label>
           <div class="col-sm-10">          
-            <input type="number" class="form-control" placeholder="Enter Id number" name="IDnumber" required>
+            <input type="number" class="form-control" placeholder="Enter Id number" name="id_num" required>
           </div>
       </div>
        <div class="form-group row">
           <label class="control-label col-sm-2">Course and Year</label>
           <div class="col-sm-10">          
-            <input type="text" class="form-control" placeholder="Enter course and year" name="course_and_year" required>
+            <input type="text" class="form-control" placeholder="Enter course and year" name="course_year" required>
           </div>
       </div>
       <div class="form-group row">
@@ -41,7 +41,7 @@
        <div class="form-group row">
           <label class="control-label col-sm-2" >Date of Birth</label>
           <div class="col-sm-10">          
-            <input type="date" class="form-control" placeholder="Enter height" name="date_of_birth" required>
+            <input type="date" class="form-control" placeholder="Enter height" name="birthdate" required>
           </div>
       </div> 
      <div class="form-group row">
@@ -59,14 +59,19 @@
       <div class="form-group row">
           <label class="control-label col-sm-2" >Years of Experience</label>
           <div class="col-sm-10">          
-            <input type="text" class="form-control" placeholder="Enter Years of Experience" name="years_of_experience" required>
+            <input type="text" class="form-control" placeholder="Enter Years of Experience" name="years_experience" required>
           </div>
       </div> 
       <div class="form-group row">
           <label class="control-label col-sm-2" >Height(in feet)</label>
           <div class="col-sm-10">          
             <input type="number" class="form-control" placeholder="Enter height" name="height" required>
- 
+          </div>
+      </div> 
+        <div class="form-group row">
+          <label class="control-label col-sm-2" >Weight(in feet)</label>
+          <div class="col-sm-10">          
+            <input type="number" class="form-control" placeholder="Enter weight" name="weight" required>
           </div>
       </div> 
      
