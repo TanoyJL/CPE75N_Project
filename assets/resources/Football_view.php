a5b41b7c61687315a7a8477610b4feb29b36c6f6
John Roble <roble_john@outlook.com>
2017-10-19 11:29:52 +0800

    Registration/Try-out form design

---
+++ assets/resources/Football_view.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Football Try Out Form</title>
+</head>
+<body>
+	<?php echo form_open('football/create'); ?>
+	<h1>Try-Out Form</h1>
+		First Name: <input type="text" name="first_name" id="first_name" /><br />
+			Last Name: <input type="text" name="last_name" id="last_name" /><br />
+			Id Number: <input type="text" name="id_num" id="id_num" /><br />
+			Course and Year: <input type="text" name="course_year" id="course_year" /><br />
+			Age: <input type="number" name="age" id="age" /><br /> 
+			Player Date of Birth: <input type="date" name="birthdate" id="birthdate" /><br />
+			Contact Phone Number: <input type="number" name="phone_number" id="phone_number" /><br />
+			Contact E-mail:	 <input type="text" name="email" id="email" /><br /><br />
+			Address: <input type="text" name="address" id="address" /><br />
+			Years of Playing Experience: <input type="number" name="year_experience" id="year_experience" /><br />
+			Height(ft): <input type="text" name="height" id="height" /><br />
+			Weight(kg): <input type="text" name="weight" id="weight" /><br />
+			Kicking Foot: 
+			  <input type="radio" name="kickingfoot" value="right" checked> Right
+			  <input type="radio" name="kickingfoot" value="left"> Left
+			  <input type="radio" name="kickingfoot" value="both"> Both
+			<br />
+			Player's Position: <br />
+			<input type="radio" name="player_position" value="goal keeper"> Goal Keeper
+			  <input type="radio" name="player_position" value="defense"> Defense
+			  <input type="radio" name="player_position" value="mid field"> Mid Field
+			  <input type="radio" name="player_position" value="forward"> Forward<br>
+			Playing Experience: <br>
+			  <input type="checkbox" name="player_experience" value="National Team"> National Team
+			  <input type="checkbox" name="player_experience" value="National PRISAA"> National PRISAA
+			  <input type="checkbox" name="player_experience" value="Regional PRISAA"> Regional PRISAA<br>
+			  <input type="checkbox" name="player_experience" value="UNI Games"> UNI Games
+			  <input type="checkbox" name="player_experience" value="Philippine National Games"> Philippine National Games
+			  <input type="checkbox" name="player_experience" value="Palarong Pambansa"> Palarong Pambansa<br>
+			  <input type="checkbox" name="player_experience" value="CVIRAA"> CVIRAA
+			  <input type="checkbox" name="player_experience" value="High School Only"> High School Only<br>
+			<br />
+			Rank Achievement of Experience: <br /> <textarea name="rank_experience" rows="5" cols="50">Write Briefly.
+			</textarea>
+			<input type="submit" value="Submit">
+			<?php echo form_close(); ?>
+			<hr />
+
+			
+</body>
+</html>
